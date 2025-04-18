467f56279f954908e25dabc28d5a9f66ff7692de
Zkuu <142339560+zkuu1@users.noreply.github.com>
2025-04-18 23:30:10 +0700

    [+] belajar variable

---
+++ go.mod
@@ -0,0 +1,3 @@
+module belajar-go
+
+go 1.23.4
---
+++ variable/variable.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+    var nama string = "Yuki"
+    umur := 21
+    var tinggi float64 = 165.5
+
+    fmt.Println("Nama:", nama)
+    fmt.Println("Umur:", umur)
+    fmt.Println("Tinggi:", tinggi)
+}
